18e4b99bcc45da92ebd3b9a5598e8ad4db3fad21
Inna <serebryakovainna@mail.ru>
2022-10-28 21:56:40 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        byte dinnerTable = 1;
+        System.out.println("Значение переменной " + "dinnerTable " + "с типом " + "byte " + "равно " + dinnerTable);
+        short armchair = 2;
+        System.out.println("Значение переменной " + "armchair " + "с типом " + "short " + "равно " + armchair);
+        int chair = 6;
+        System.out.println("Значение переменной " + "chair " + "с типом " + "int " + "равно " + chair);
+        long lamp = 10;
+        System.out.println("Значение переменной " + "lamp " + "с типом " + "long " + "равно " + lamp);
+    }
+}
